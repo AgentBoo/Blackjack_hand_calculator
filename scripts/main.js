70a2297a34ed4530fb39439f5ef92684f70957e3
AgentBoo <zuzi.han@gmail.com>
2017-08-07 23:14:50 -0400

    daily project 11 complete submission

--- scripts/main.js
+++ scripts/main.js
@@ -5,15 +5,33 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
 
 function handValue (hand) {
+  let h = hand;
+  let n = 0, f = 0, aOne = 0, aEleven = 0;
 
+  for (let i = 0; i < h.length; i++){
+    if (h[i] == "A"){
+      aOne += 1;
+      aEleven += 11;
+    } else if (h[i]== "J" || h[i] == "Q" || h[i] == "K"){
+      f += 10;
+    } else {
+      n += Number(h[i]);
+    }
+  } const sum1 = n + f + aOne;
+    const sum2 = n + f + aEleven;
 
-  return;
+  if (sum2 > sum1 && sum2 <= 21){
+    return sum2;
+  } else {
+    return sum1;
+  }
+
 }
 
 
